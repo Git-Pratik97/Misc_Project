3467a84b6caa1c779bd1ed3f7f384e8180f4905f
Pratik Kamble <62785111+Git-Pratik97@users.noreply.github.com>
2023-02-03 14:28:02 +0530

    Add files via upload
    Creating a new Trip Planner Application for Office Employees...

---
+++ src/main/java/com/example/tripPlanner/TripPlannerApplication.java
@@ -0,0 +1,14 @@
+package com.example.tripPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripPlannerApplication {
+
+	public static void main(String[] args) {
+//		System.out.println("Hello Testing");
+		SpringApplication.run(TripPlannerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/tripPlanner/dto/UserDTO.java
@@ -0,0 +1,10 @@
+package com.example.tripPlanner.dto;
+
+import java.util.Date;
+
+public class UserDTO {
+    private long id;
+    private Date date;
+    private 
+
+}
